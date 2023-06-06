46b38faebadb532ebbe391fdc19d4464499cca21
t2minht <tuongtran0240@gmail.com>
2023-06-06 17:47:33 -0500

    resized text inputs, added borders, started on mood bar

---
+++ components/Button.js
@@ -0,0 +1,38 @@
+import { StyleSheet, View, Pressable, Text } from 'react-native';
+
+export default function Button({ label }) {
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable style={styles.button} onPress={() => alert('You pressed ' + {label} )}>
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3,
+    borderColor: '#000000',
+    borderWidth: '2px',
+  },
+  button: {
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  buttonIcon: {
+    paddingRight: 8,
+  },
+  buttonLabel: {
+    color: '#000000',
+    fontSize: 16,
+  },
+});
