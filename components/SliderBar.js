b4e1da9ab5e6f62488f11bec3735d75d86b41b8e
t2minht <tuongtran0240@gmail.com>
2023-06-07 11:52:00 -0500

    deleted button and moved content to sliderbar

--- components/SliderBar.js
+++ components/SliderBar.js
@@ -1,22 +1,43 @@
 import { StyleSheet, Text, View, Pressable } from 'react-native';
-import Button from './Button';
 
 export default function SliderBar() {
     const x = 0; 
 
     return (
         <View style={[styles.container, {flexDirection: 'row'} ]}>
-            <Button label = {0} style={styles.button}/>
-            <Button label = {1} style={styles.button}/>
-            <Button label = {2} style={styles.button}/>
-            <Button label = {3} style={styles.button}/>
-            <Button label = {4} style={styles.button}/>
-            <Button label = {5} style={styles.button}/>
-            <Button label = {6} style={styles.button}/>
-            <Button label = {7} style={styles.button}/>
-            <Button label = {8} style={styles.button}/>
-            <Button label = {9} style={styles.button}/>
-            <Button label = {10} style={styles.button}/>
+            <Pressable style={styles.leftbutton} onPress={() => alert(0)}>
+                <Text style={styles.buttonLabel}>{0}</Text>
+            </Pressable>
+            <Pressable style={styles.button} onPress={() => alert(0)}>
+                <Text style={styles.buttonLabel}>{1}</Text>
+            </Pressable>
+            <Pressable style={styles.button} onPress={() => alert(0)}>
+                <Text style={styles.buttonLabel}>{2}</Text>
+            </Pressable>
+            <Pressable style={styles.button} onPress={() => alert(0)}>
+                <Text style={styles.buttonLabel}>{3}</Text>
+            </Pressable>
+            <Pressable style={styles.button} onPress={() => alert(0)}>
+                <Text style={styles.buttonLabel}>{4}</Text>
+            </Pressable>
+            <Pressable style={styles.button} onPress={() => alert(0)}>
+                <Text style={styles.buttonLabel}>{5}</Text>
+            </Pressable>
+            <Pressable style={styles.button} onPress={() => alert(0)}>
+                <Text style={styles.buttonLabel}>{6}</Text>
+            </Pressable>
+            <Pressable style={styles.button} onPress={() => alert(0)}>
+                <Text style={styles.buttonLabel}>{7}</Text>
+            </Pressable>
+            <Pressable style={styles.button} onPress={() => alert(0)}>
+                <Text style={styles.buttonLabel}>{8}</Text>
+            </Pressable>
+            <Pressable style={styles.button} onPress={() => alert(0)}>
+                <Text style={styles.buttonLabel}>{9}</Text>
+            </Pressable>
+            <Pressable style={styles.rightbutton} onPress={() => alert(0)}>
+                <Text style={styles.buttonLabel}>{10}</Text>
+            </Pressable>
         </View>
     );
 }
@@ -30,17 +51,33 @@ export default function SliderBar() {
         borderRadius: 25
     },
     leftbutton:{
-
+        flex: 1,
+        width: '100%',
+        height: 60,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        marginLeft:5,
     },
     button: {
         flex: 1,
         width: '100%',
-        height: '100%',
+        height: 60,
         alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'row',
     },
     rightbutton:{
-        
+        flex: 1,
+        width: '100%',
+        height: 60,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        marginRight:5,
+    },
+    buttonLabel: {
+      color: '#000000',
+      fontSize: 16,
     },
   });
